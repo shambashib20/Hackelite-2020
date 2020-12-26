6f7552770c2580d5d673b09debec3be4b4eab289
Aman Chopra <56465233+Aman1905@users.noreply.github.com>
2020-12-26 16:37:57 +0530

    updated

--- src/Cosmetics.js
+++ src/Cosmetics.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { Link } from "react-router-dom"
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import IconButton from '@material-ui/core/IconButton';
 
-function Cosmetics() {
-    return (
-        <div>
-            <h1>Cosmetics</h1>
-        </div>
-    )
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: theme.spacing(149),
+  },
+}));
+
+export default function Cosmetics() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="fixed">
+        <Toolbar variant="dense">
+          <Typography variant="h6" color="inherit">
+          <Link to="/" style={{ textDecoration: 'none' , color: 'white'}}> Precess </Link>
+          </Typography>
+          <IconButton edge="end" className={classes.menuButton} color="inherit" aria-label="menu">
+            <ShoppingCartIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
-
-export default Cosmetics
