42c71b55face1dad9a41cb82e700521e55a7aed7
Aman Chopra <56465233+Aman1905@users.noreply.github.com>
2020-12-26 21:24:00 +0530

    updated #3

--- src/Stationary.js
+++ src/Stationary.js
@@ -6,6 +6,11 @@ import Typography from '@material-ui/core/Typography';
 import { Link } from "react-router-dom"
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import IconButton from '@material-ui/core/IconButton';
+import Carousel from 'react-bootstrap/Carousel'
+import Stationary1 from "./StationaryItems/Stationary1"
+import Stationary2 from "./StationaryItems/Stationary2"
+import Stationary3 from "./StationaryItems/Stationary3"
+import Stationary4 from "./StationaryItems/Stationary4"
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -31,6 +36,55 @@ export default function Stationary() {
           </IconButton>
         </Toolbar>
       </AppBar>
+      <br />  <br />  <br /> 
+      <div className="clothes-1">
+      <Stationary1 /> {' '} {' '}
+      <Stationary2 />
+      </div>
+      <br /> <br />
+      <div className="clothes-2">
+      <Stationary3  />
+      <Stationary4  />
+      </div>
+{/*     
+    <br /> <br /> <br />
+    <Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://images.unsplash.com/photo-1473773508845-188df298d2d1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTZ8fG5hdHVyZXxlbnwwfDB8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://images.unsplash.com/photo-1473773508845-188df298d2d1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTZ8fG5hdHVyZXxlbnwwfDB8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://images.unsplash.com/photo-1473773508845-188df298d2d1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTZ8fG5hdHVyZXxlbnwwfDB8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel> */}
     </div>
   );
 }
