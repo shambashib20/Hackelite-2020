f1d415021946039852f05b21f2db5420febe4d21
Aman Chopra <56465233+Aman1905@users.noreply.github.com>
2020-12-27 14:07:33 +0530

    updated ##

--- src/ClothingItems/Cloth2.js
+++ src/ClothingItems/Cloth2.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { useStateValue } from '../StateProvider'
 import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
@@ -13,9 +14,12 @@ import Typography from '@material-ui/core/Typography';
 import { red } from '@material-ui/core/colors';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import PhoneInTalkIcon from '@material-ui/icons/PhoneInTalk';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
   root: {
+    minWidth:300,
     maxWidth: 345,
   },
   media: {
@@ -37,13 +41,27 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function Cloth1() {
+export default function Cloth1({ brand, size, used, price, additional }) {
   const classes = useStyles();
   const [expanded, setExpanded] = React.useState(false);
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
+  const [{basket}, dispatch] = useStateValue();
+
+  const addToBasket = () => {
+      dispatch ({
+        type: "ADD_TO_BASKET",
+        item: {
+          brand:brand,
+           size: size,
+            used: used,
+             price: price,
+              additional : additional,
+        }
+      })
+    };
 
   return (
     <Card className={classes.root}>
@@ -62,15 +80,10 @@ export default function Cloth1() {
         image="https://i.imgur.com/tdoXnpH.jpg"
         title="short skirt"
       />
-      <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae deleniti blanditiis maxime totam voluptates repudiandae? Eveniet, sapiente quos ipsum magnam molestias possimus vero enim alias.
-        </Typography>
-      </CardContent>
+      
       <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <ShoppingCartIcon />
-        </IconButton>
+      <Button onClick={addToBasket}><ShoppingCartIcon /></Button> 
+      <Button ><PhoneInTalkIcon /></Button>
         <IconButton
           className={clsx(classes.expand, {
             [classes.expandOpen]: expanded,
--- src/ClothingItems/Cloth3.js
+++ src/ClothingItems/Cloth3.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { useStateValue } from '../StateProvider'
 import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
@@ -13,9 +14,12 @@ import Typography from '@material-ui/core/Typography';
 import { red } from '@material-ui/core/colors';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import PhoneInTalkIcon from '@material-ui/icons/PhoneInTalk';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
   root: {
+    minWidth:300,
     maxWidth: 345,
     marginRight: 316,
   },
@@ -38,13 +42,27 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function Cloth1() {
+export default function Cloth1({ brand, size, used, price, additional }) {
   const classes = useStyles();
   const [expanded, setExpanded] = React.useState(false);
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
+  const [{basket}, dispatch] = useStateValue();
+
+  const addToBasket = () => {
+      dispatch ({
+        type: "ADD_TO_BASKET",
+        item: {
+          brand:brand,
+           size: size,
+            used: used,
+             price: price,
+              additional : additional,
+        }
+      })
+    };
 
   return (
     <Card className={classes.root}>
@@ -55,22 +73,18 @@ export default function Cloth1() {
           </Avatar>
         }
         
-        title="Skirt"
-        subheader="ID - #4758733"
+        title="Short Skirt"
+        subheader="ID - #4446433"
       />
       <CardMedia
         className={classes.media}
         image="https://i.imgur.com/urJo7EG.jpg"
+        title="short skirt"
       />
-      <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae deleniti blanditiis maxime totam voluptates repudiandae? Eveniet, sapiente quos ipsum magnam molestias possimus vero enim alias.
-        </Typography>
-      </CardContent>
+      
       <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <ShoppingCartIcon />
-        </IconButton>
+      <Button onClick={addToBasket}><ShoppingCartIcon /></Button> 
+      <Button ><PhoneInTalkIcon /></Button>
         <IconButton
           className={clsx(classes.expand, {
             [classes.expandOpen]: expanded,
@@ -86,7 +100,7 @@ export default function Cloth1() {
         <CardContent>
           <Typography>
           <h3>
-            Brand: Forever 21 <br />
+          Brand: Forever 21 <br />
             Size: S <br />
             Used: yes <br />
             Price: 500 <br />
