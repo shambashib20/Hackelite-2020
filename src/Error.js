f3ab9bf73e3354b401b5910002d3bd9365715586
Aman Chopra <56465233+Aman1905@users.noreply.github.com>
2020-12-27 09:06:29 +0530

    update #2

--- src/Error.js
+++ src/Error.js
@@ -3,6 +3,7 @@ import React from 'react'
 function Error() {
     return (
         <div>
+        //this is an error page
             <h1>Ooops!! Page not found </h1>
 
             <h2> Error 404 !!!</h2>
